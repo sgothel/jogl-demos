d38bf164669f6a9fb924f3305e6917e8c33de402
Kenneth Russel <kbrussel@alum.mit.edu>
2005-11-28 01:02:55 +0000

    Added copyright notice and attribution to XTrans demo
    git-svn-id: file:///usr/local/projects/SUN/JOGL/git-svn/../svn-server-sync/jogl-demos/trunk@150 3298f667-5e0e-4b4a-8ed4-a3559d26a5f4

--- src/demos/xtrans/OffscreenComponentWrapper.java
+++ src/demos/xtrans/OffscreenComponentWrapper.java
@@ -1,3 +1,42 @@
+/*
+ * Copyright (c) 2005 Sun Microsystems, Inc. All Rights Reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * - Redistribution of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * 
+ * - Redistribution in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * 
+ * Neither the name of Sun Microsystems, Inc. or the names of
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * This software is provided "AS IS," without a warranty of any kind. ALL
+ * EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES,
+ * INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN
+ * MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL NOT BE LIABLE FOR
+ * ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR
+ * DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN OR
+ * ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR
+ * DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE
+ * DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY,
+ * ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF
+ * SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ * 
+ * You acknowledge that this software is not designed or intended for use
+ * in the design, construction, operation or maintenance of any nuclear
+ * facility.
+ * 
+ * Sun gratefully acknowledges that this software was originally authored
+ * and developed by Kenneth Bradley Russell and Christopher John Kline.
+ */
+
 package demos.xtrans;
 
 import java.awt.*;
@@ -9,14 +48,17 @@ import com.sun.opengl.impl.Debug;
 import com.sun.opengl.impl.Java2D;
 
 /** Provides an interposition point where we can install a new
-    Graphics object in the rendering pipeline. Because lightweight
-    components always delegate up to their parents to fetch Graphics
-    objects, we can use this point to swap in the off-screen
-    VolatileImage we're using for rendering. Applications should not
-    need to construct instances of this class directly, though they
-    may encounter them when traversing the component hierarchy created
-    by the OffscreenDesktopPane, since they are automatically inserted
-    during add operations. */
+ * Graphics object in the rendering pipeline. Because lightweight
+ * components always delegate up to their parents to fetch Graphics
+ * objects, we can use this point to swap in the off-screen
+ * VolatileImage we're using for rendering. Applications should not
+ * need to construct instances of this class directly, though they may
+ * encounter them when traversing the component hierarchy created by
+ * the OffscreenDesktopPane, since they are automatically inserted
+ * during add operations.
+ *
+ * @author Kenneth Russell
+ */
 
 public class OffscreenComponentWrapper extends JComponent {
   private static final boolean DEBUG = Debug.debug("OffscreenComponentWrapper");
