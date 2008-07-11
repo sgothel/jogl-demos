f36d2e40bb13d29dddd8baa0e24df569c50c8202
Sven Gothel <sgothel@jausoft.com>
2008-07-11 14:29:19 +0000

    Using jogl-demos.jar instead of the classes dir
    git-svn-id: file:///usr/local/projects/SUN/JOGL/git-svn/../svn-server-sync/jogl-demos/branches/JOGL_2_SANDBOX@259 3298f667-5e0e-4b4a-8ed4-a3559d26a5f4

--- setenv-jogl.sh
+++ setenv-jogl.sh
@@ -31,7 +31,7 @@ GLUEGEN_OS=$THISDIR/../gluegen/build/obj
 LIB=$THISDIR/lib
 
 # CLASSPATH=$JAVA_HOME/jre/lib/rt.jar:.:build/classes
-CLASSPATH=.:$THISDIR/build/classes:$GLUEGEN_JAR:$JOGL_CLASSPATH
+CLASSPATH=.:$THISDIR/build/jogl-demos.jar:$GLUEGEN_JAR:$JOGL_CLASSPATH
 for i in $LIB/*jar ; do
     CLASSPATH=$CLASSPATH:$i
 done
