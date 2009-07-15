7af34608611659680f37025ee68773c254518f06
sg215889 <sg215889@phalanx.goethel.localnet>
2009-07-15 07:17:49 -0700

    Add cpu flag to fix broken CVM os.arch (here on linux)

--- cvm-run-newt.sh
+++ cvm-run-newt.sh
@@ -3,16 +3,20 @@
 CVM=$1
 shift
 
-JOGL_LIB_DIR=$1
-shift
-
 BUILD_SUBDIR=$1
 shift
 
-if [ ! -x "$CVM" -o -z "$JOGL_LIB_DIR" -o -z "$BUILD_SUBDIR" ] ; then
-    echo Usage $0 CVM-Binary JOGL_LIB_DIR BUILD_SUB_PATH
-    echo e.g. $0 ../CVM/bin/cvm ../jogl/build-cdcfp-x86/lib build-cdcfp-x86
+if [ ! -x "$CVM" -o -z "$BUILD_SUBDIR" ] ; then
+    echo "Usage $0 CVM-Binary BUILD_SUB_PATH [-cpu <arch>] Main-Class"
+    echo "e.g. $0 ../CVM/bin/cvm build-x86 -cpu x86 test"
     exit 1
 fi
 
-$CVM -Djava.awt.headless=true -Dsun.boot.library.path=$JOGL_LIB_DIR -Xbootclasspath/a:../gluegen/$BUILD_SUBDIR/gluegen-rt.jar -Xbootclasspath/a:../jogl/$BUILD_SUBDIR/nativewindow/nativewindow.core.jar -Xbootclasspath/a:../jogl/$BUILD_SUBDIR/jogl/jogl.cdcfp.jar -Xbootclasspath/a:../jogl/$BUILD_SUBDIR/newt/newt.cdcfp.jar -Xbootclasspath/a:$BUILD_SUBDIR/jogl-demos.jar com.sun.javafx.newt.util.MainThread $* 2>&1 | tee cvm-dbg-newt.log
+X_FLAGS=
+if [ "$1" = "-cpu" ] ; then
+    shift
+    X_FLAGS="-Dos.arch=$1"
+    shift
+fi
+
+$CVM $X_FLAGS -Djava.awt.headless=true -Dsun.boot.library.path=../jogl/$BUILD_SUBDIR/lib -Xbootclasspath/a:../gluegen/$BUILD_SUBDIR/gluegen-rt-cdc.jar -Xbootclasspath/a:../jogl/$BUILD_SUBDIR/nativewindow/nativewindow.core.jar -Xbootclasspath/a:../jogl/$BUILD_SUBDIR/jogl/jogl.all.cdc.jar -Xbootclasspath/a:../jogl/$BUILD_SUBDIR/newt/newt.all.cdc.jar -Xbootclasspath/a:$BUILD_SUBDIR/jogl-demos.jar com.sun.javafx.newt.util.MainThread $* 2>&1 | tee cvm-run-newt.log
