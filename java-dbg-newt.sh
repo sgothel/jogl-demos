fb8f84f8da722154558035c51a6a2ac57841324d
sg215889 <sg215889@jordan.goethel.localnet>
2009-07-30 02:02:24 -0700

    Fix www documentation

--- java-dbg-newt.sh
+++ java-dbg-newt.sh
@@ -15,7 +15,7 @@ if [ $CPOK -eq 0 ] ; then
     echo No JOGL in CLASSPATH
 else
     # D_ARGS="-Dgluegen.debug.ProcAddressHelper=true -Dgluegen.debug.NativeLibrary=true -Dnativewindow.debug=all -Djogl.debug=all -Dnewt.debug=all"
-    D_ARGS="-Dgluegen.debug.NativeLibrary=true -Dnativewindow.debug=all -Djogl.debug=all -Dnewt.debug=all"
+    D_ARGS="-Dnativewindow.debug=all -Djogl.debug=all -Dnewt.debug=all"
     if [ $MOSX -eq 1 ] ; then
         X_ARGS="-XstartOnFirstThread"
     fi
