08d7de9c49d6bd778d0a915731ae9edbafdf646c
Sven Gothel <sgothel@jausoft.com>
2009-11-10 03:42:07 +0100

    Redbook seperation ..

--- make/make.jogl.all.x86_64.sh
+++ make/make.jogl.all.x86_64.sh
@@ -4,8 +4,10 @@
 
 #    -Dc.compiler.debug=true 
 #    -Djogl.cg=1 \
+#    -Djogl.redbook=true \
 
 ant \
     -Djogl.cg=1 \
+    -Duser.swt.jar=$HOME/.java/swt.jar \
     -Drootrel.build=build-x86_64 \
     $* 2>&1 | tee make.jogl.all.x86_64.log
