1002133ac28688df17f3e3ed6e44a8bb578cbfb5
Mark Raynsford <mark@io7m.com>
2013-01-07 15:07:26 +0000

    Use API level 10 (as the SDK will now refuse to use 9 due to it being replaced with 10), and add a nice jp4da icon

--- maven/jp4da/jp4da-android/gen/com/io7m/examples/jp4da/R.java
+++ maven/jp4da/jp4da-android/gen/com/io7m/examples/jp4da/R.java
@@ -10,10 +10,13 @@ package com.io7m.examples.jp4da;
 public final class R {
     public static final class attr {
     }
+    public static final class drawable {
+        public static final int ic_launcher=0x7f020000;
+    }
     public static final class layout {
-        public static final int main=0x7f020000;
+        public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
     }
 }
