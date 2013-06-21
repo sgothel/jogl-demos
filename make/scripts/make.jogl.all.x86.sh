f8918d4e8eb6a598deb9e01566e903c98c40fcf7
Sven Gothel <sgothel@jausoft.com>
2013-06-21 10:11:38 +0200

    make script 32bit: force java6 creation

--- make/scripts/make.jogl.all.x86.sh
+++ make/scripts/make.jogl.all.x86.sh
@@ -11,6 +11,9 @@ fi
 #    -Dc.compiler.debug=true 
 
 ant \
+    -Dtarget.sourcelevel=1.6 \
+    -Dtarget.targetlevel=1.6 \
+    -Dtarget.rt.jar=/opt-share/jre1.6.0_30/lib/rt.jar \
     -Djogl.cg=1 \
     -Duser.swt.jar=$SWT_CLASSPATH \
     -Drootrel.build=build-x86 \
