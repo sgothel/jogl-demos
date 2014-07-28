1bfb3f2eb9e0b3a088c2762c05629a7913e06b21
Sven Gothel <sgothel@jausoft.com>
2014-07-28 19:38:22 +0200

    Adapt to JOGL commit 15b9e36e80d6f62f7dfb5c45d00cd04de2007ee5 (GammaRamp changes)

--- src/demos/gamma/TestGamma.java
+++ src/demos/gamma/TestGamma.java
@@ -1,21 +1,21 @@
 /*
  * Copyright (c) 2005 Sun Microsystems, Inc. All Rights Reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
- * 
+ *
  * - Redistribution of source code must retain the above copyright
  *   notice, this list of conditions and the following disclaimer.
- * 
+ *
  * - Redistribution in binary form must reproduce the above copyright
  *   notice, this list of conditions and the following disclaimer in the
  *   documentation and/or other materials provided with the distribution.
- * 
+ *
  * Neither the name of Sun Microsystems, Inc. or the names of
  * contributors may be used to endorse or promote products derived from
  * this software without specific prior written permission.
- * 
+ *
  * This software is provided "AS IS," without a warranty of any kind. ALL
  * EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES,
  * INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A
@@ -28,25 +28,29 @@
  * DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY,
  * ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF
  * SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
- * 
+ *
  * You acknowledge that this software is not designed or intended for use
  * in the design, construction, operation or maintenance of any nuclear
  * facility.
- * 
+ *
  * Sun gratefully acknowledges that this software was originally authored
  * and developed by Kenneth Bradley Russell and Christopher John Kline.
  */
 
 package demos.gamma;
 
-import demos.gears.Gears;
 import java.awt.Frame;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import javax.media.opengl.*;
+
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLEventListener;
 import javax.media.opengl.awt.GLCanvas;
+
 import com.jogamp.opengl.util.Animator;
 import com.jogamp.opengl.util.Gamma;
+
+import demos.gears.Gears;
 
 
 
@@ -56,24 +60,26 @@ public class TestGamma implements GLEventListener {
     System.exit(1);
   }
 
-  public void init(GLAutoDrawable drawable) {
-    GL gl = drawable.getGL();
-    if (!Gamma.setDisplayGamma(gl, gamma, brightness, contrast)) {
+  @Override
+public void init(GLAutoDrawable drawable) {
+    if (!Gamma.setDisplayGamma(drawable, gamma, brightness, contrast)) {
       System.err.println("Unable to change display gamma, brightness, and contrast");
     }
     System.err.println("init: Gamma.setDisplayGamma");
   }
 
-  public void dispose(GLAutoDrawable drawable) {
-    GL gl = drawable.getGL();
-    Gamma.resetDisplayGamma(gl);
+  @Override
+public void dispose(GLAutoDrawable drawable) {
+    Gamma.resetDisplayGamma(drawable);
     System.err.println("dispose: Gamma.resetDisplayGamma");
   }
 
-  public void display(GLAutoDrawable drawable) {
+  @Override
+public void display(GLAutoDrawable drawable) {
   }
 
-  public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+  @Override
+public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
   }
 
   public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
@@ -100,12 +106,14 @@ public class TestGamma implements GLEventListener {
     frame.setSize(300, 300);
     final Animator animator = new Animator(canvas);
     frame.addWindowListener(new WindowAdapter() {
-        public void windowClosing(WindowEvent e) {
+        @Override
+		public void windowClosing(WindowEvent e) {
           // Run this on another thread than the AWT event queue to
           // make sure the call to Animator.stop() completes before
           // exiting
           new Thread(new Runnable() {
-              public void run() {
+              @Override
+			public void run() {
                 animator.stop();
                 System.exit(0);
               }
@@ -114,7 +122,7 @@ public class TestGamma implements GLEventListener {
       });
     frame.setVisible(true);
     animator.start();
-    
+
   }
 
     static float gamma = 1.0f;
