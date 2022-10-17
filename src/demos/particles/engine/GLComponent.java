9e87b3f07de841ee90c11b8d367ac5d889af1c46
Sven Gothel <sgothel@jausoft.com>
2022-10-17 23:29:07 +0200

    Applying author's privacy request of src/demos/particles/engine/* files

--- src/demos/particles/engine/GLComponent.java
+++ src/demos/particles/engine/GLComponent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006 Ben Chappell (bwchappell@gmail.com) All Rights Reserved.
+ * Copyright (c) 2006 Ben Chappell. All Rights Reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
